49e5bddd28694ae3c7597fd5f98494bf3a9c8afe
ayadij <ayadijkwel@yahoo.com>
2018-04-04 13:21:17 -0600

    const variable

---
+++ const.js
@@ -0,0 +1,15 @@
+
+
+// old method
+var city = 'Scottsdale';
+conosle.log(city);
+
+
+// does not pollute the global namespace // more specific
+let city = 'Scottsdale';
+console.log(city);
+
+
+// newest member to the variable family in javascript 
+const city = 'Scottsdale';
+console.log(city);
