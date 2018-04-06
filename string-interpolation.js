3d1125ac5d7e053e6c240e800817be3174a4e84d
ayadij <ayadijkwel@yahoo.com>
2018-04-06 16:26:08 -0600

    string interpolation

--- string-interpolation.js
+++ string-interpolation.js
@@ -1,7 +1,10 @@
+// https://rails.devcamp.com/modern-javascript/basics-modern-javascript-development/guide-backtick-string-interpolation-javascript
+
+
 
 // the old way
-console.log("I'm " + 24);
+console.log("I'm " + 23);
 
 // modern way  // much like ruby and other modern languages
-const age = 10;
-console.log("I will be ${age + 1} next year.");
+const age = 23;
+console.log(`I will be ${age + 1} next year.`);
